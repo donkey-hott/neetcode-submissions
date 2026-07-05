2feca988235ecb6ade76b32784e02ac38aee1ede
Kanstantsin Lazouski <60788110+donkey-hott@users.noreply.github.com>
2026-07-05 17:53:48 +0200

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hashmap = defaultdict(int)
+
+        for n in nums:
+            hashmap[n] += 1
+            if hashmap[n] > len(nums) // 2: return n
+
+        return -1
