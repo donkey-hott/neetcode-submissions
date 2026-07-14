4d504ffbea0e706b7345daf2a7c2133693aa3392
Kanstantsin Lazouski <60788110+donkey-hott@users.noreply.github.com>
2026-07-14 22:05:43 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        output = ""
+
+        for s in strs:
+            prefix = f':{len(s)}:'
+            output += prefix
+
+            for char in s:
+                output += char
+        return output
+
+    def decode(self, s: str) -> List[str]:
+        print(s)
+        word_len = 0
+        i = 0
+        decoded = []
+        while i < len(s):
+            if s[i] == ':':
+                p = i + 1
+                while s[p] != ':':
+                    p += 1
+                word_len = int(s[i+1:p])
+                i = p + 1
+
+            word = ''
+
+            while len(word) < word_len:
+                word += s[i]
+                i += 1
+            decoded.append(word)
+        return decoded
