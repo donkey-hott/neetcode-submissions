99d1a89bca2977abbb2c6863cb81e493fc2e7deb
Kanstantsin Lazouski <60788110+donkey-hott@users.noreply.github.com>
2026-09-06 22:25:26 +0200

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        frequencies = defaultdict(int)
+        l, n = 0, len(s2)
+
+        for c in s1:
+            frequencies[c] += 1
+        
+        for l in range(n):
+            if s2[l] in frequencies:
+                r = l
+                frequencies_c = frequencies.copy()
+                while r < n and s2[r] in frequencies_c and frequencies_c[s2[r]] > 0:
+                    frequencies_c[s2[r]] -= 1
+                    r += 1
+
+                if all(v == 0 for v in frequencies_c.values()):
+                    return True
+                l = r
+        return False
